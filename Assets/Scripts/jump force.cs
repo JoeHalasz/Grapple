b74ffcf109252f63f59f86078f58ec08a5dfe0b2
cqbalt <75058047+cqbalt@users.noreply.github.com>
2024-09-21 20:27:48 -0500

    trash

---
+++ Assets/Scripts/jump force.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jumpforce : MonoBehaviour
+{
+    Rigidbody2D body; 
+    // Start is called before the first frame update
+    void Start()
+    {
+      body =  GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            body.AddForce(new Vector2(0, 9000));
+        }
+
+        Debug.Log(body.position);
+    }
+}
